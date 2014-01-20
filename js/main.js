edb95953ba91b43484dc7fe3b1e0ca4d8acc2e7e
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-19 18:10:12 -0800

    Update CSS for current topic box

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
+//Questions
+//1. Why do I have to set newItemName and newItemTopic to "" in order for the error message to be generated the first time around? Shouldn't they already equal ""? Or should I test for "null" in my if statement?
+
 function QappCtrl ($scope) {
   $scope.items = [
     {name: "Kyle", topic: "Controllers in Angular", time: "25 minutes"},
     {name: "Arya", topic: "CSS relative positioning", time: "10 minutes"},
     {name: "Eva", topic: "Implementing game logic in Angular", time: "35 minutes"}, 
   ]
-  
+
+  $scope.newItemName = ""
+  $scope.newItemTopic = ""
   $scope.newItemTime = ""
   $scope.addItem = function() {
-    if ($scope.newItemTime == "") {
-      $scope.items.push({name: $scope.newItemName, topic: $scope.newItemTopic, time: "Unestimated"})
-      $scope.newItemName = "";
-      $scope.newItemTopic = "";
-    } else {
-      $scope.items.push({name: $scope.newItemName, topic: $scope.newItemTopic, time: $scope.newItemTime + " minutes"})
-      $scope.newItemName = "";
-      $scope.newItemTopic = "";
-      $scope.newItemTime = "";
+    if ($scope.newItemName == "" || $scope.newItemTopic == "") {
+      $scope.errorMessage = "Whoops! Please include your name and the topic  :]"
+    }
+    else {
+      if ($scope.newItemTime == "") {
+        $scope.items.push({name: $scope.newItemName, topic: $scope.newItemTopic, time: "Unestimated"})
+        $scope.newItemName = "";
+        $scope.newItemTopic = "";
+      } else {
+        $scope.items.push({name: $scope.newItemName, topic: $scope.newItemTopic, time: $scope.newItemTime + " minutes"})
+        $scope.newItemName = "";
+        $scope.newItemTopic = "";
+        $scope.newItemTime = "";
+      }
+      $scope.errorMessage = ""
     }
   }
+  
   $scope.removeItem = function() {
     var placeHolder = $scope.items;
     $scope.items = [];
