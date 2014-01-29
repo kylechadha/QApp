6c06820bb2e36dce1f8612d6f53d0c393e3673f1
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-28 23:00:21 -0800

    Strip all timer functionality

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ app.controller("QappCtrl", function($scope, $firebase) {
     if (IDs.length == 0) {
       $scope.fbRoot.$add( {
         items: [
-          {name: 'QApp', topic: 'Add something, then clear me :]', duration: 15, estimated: '9:45 PM'}
+          {name: 'QApp', topic: 'Add something, then clear me!'}
           ]
       });
       $scope.fbRoot.$on("change", function() {
@@ -30,84 +30,23 @@ app.controller("QappCtrl", function($scope, $firebase) {
 
   $scope.newItemName = ""
   $scope.newItemTopic = ""
-  $scope.newItemTime = ""
   $scope.addItem = function() {
     if ($scope.newItemName == "" || $scope.newItemTopic == "") {
       $scope.errorMessage = "Whoops! Please include your name and the topic  :]"
     }
     else {
-      if ($scope.newItemTime == "") {
-        $scope.newItemTime = 15;
-        $scope.db.items.push({name: $scope.newItemName, topic: $scope.newItemTopic, duration: $scope.newItemTime, estimated: $scope.timeCalc()})
-      } else {
-        $scope.db.items.push({name: $scope.newItemName, topic: $scope.newItemTopic, duration: $scope.newItemTime, estimated: $scope.timeCalc()})
-      }
+      $scope.db.items.push({name: $scope.newItemName, topic: $scope.newItemTopic})
       $scope.db.$save();
       $scope.newItemName = "";
       $scope.newItemTopic = "";
-      $scope.newItemTime = "";
       $scope.errorMessage = ""
     }
-  }
-
-  var finishTime = 0;
-  var date = new Date;
-  var hour = 0;
-  var min = 0;
-
-  $scope.timeCalc = function() {
-    if ($scope.db.items.length == 0) {
-      hour = date.getHours();
-      min = date.getMinutes();
-
-      $scope.estTime = "Active";
-      finishTime = parseInt($scope.newItemTime) + parseInt(min);
-
-      return $scope.estTime;
-    } else {
-      
-      $scope.estTime = finishTime;
-      finishTime += parseInt($scope.newItemTime);
-      return $scope.timeClean($scope.estTime, hour);
-    }
-  }
-
-  $scope.timeUpdate = function() {
-    date = new Date;
-    hour = date.getHours();
-    min = date.getMinutes();
-
-    $scope.db.items[0].estimated = "Active"
-
-    for (i = 0; i < $scope.db.items.length; i++) {
-      finishTime = parseInt($scope.db.items[i].duration) + parseInt(min);
-      $scope.db.items[i+1].estimated = $scope.timeClean(finishTime, hour);
-
-      console.log($scope.db.items[i].estimated);
-      console.log($scope.db.items[i].duration);
-    }
-  }
-
-  $scope.timeClean = function(min, hour) {
-    hour += Math.floor(min / 60);
-    min = min % 60;
-
-    var ampm = hour > 12 ? "PM" : "AM";
-    hour = hour % 12;
-    hour = hour ? hour : 12; // zero = 12
-    min = min > 9 ? min : "0" + min;
-    $scope.estTime = hour + ":" + min + " " + ampm;
-
-    return $scope.estTime;
   }
 
   $scope.removeItem = function(item) {
     var itemIndex = $scope.db.items.indexOf(item);
     $scope.db.items.splice(itemIndex,1);
-    $scope.timeCalc();
     $scope.db.$save();
   }
-
-  $scope.counter = 0
 
 });
